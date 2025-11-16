14f460b099f779f2621c820a957b880b7bf27929
SantiagoSpook <santiabruh@gmail.com>
2025-11-16 10:25:49 -0600

    POST productos
    Se agrega un script en el index.js para poder agregar productos a la base de datos a traves de postman

--- index.js
+++ index.js
@@ -32,6 +32,49 @@ app.get("/api/products", async (req, res) => {
   }
 });
 
+// post producto
+app.post("/api/products", async (req, res) => {
+  const { name, description, price, stock, image } = req.body;
+
+  // validaciones
+  if (!name || !description || !price || !stock) {
+    return res.status(400).json({
+      error: "Los campos name, description, price y stock son obligatorios",
+    });
+  }
+
+  const query = `
+        INSERT INTO products (name, description, price, stock, image, created_at)
+        VALUES (?, ?, ?, ?, ?, NOW())
+    `;
+
+  try {
+    const [result] = await pool.query(query, [
+      name,
+      description,
+      price,
+      stock,
+      image || null,
+    ]);
+
+    res.status(201).json({
+      message: "Producto creado exitosamente",
+      id: result.insertId,
+      producto: {
+        id: result.insertId,
+        name,
+        description,
+        price,
+        stock,
+        image,
+      },
+    });
+  } catch (err) {
+    console.error("Error al crear producto:", err);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
